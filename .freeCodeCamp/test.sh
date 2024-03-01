7951effb8a081630913efdd45e6bc703e7b11b79
Matthew Glassman <matthewglassman78@gmail.com>
2024-03-01 02:33:50 +0000

    Save progress: 230.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 QUESTION1="What's your name?"
+QUESTION2="Where are you from?"
 
 echo $QUESTION1
 read NAME
-echo $NAME
+
+
+echo Hello $NAME.
+echo $QUESTION2
--- questionnaire.sh
+++ questionnaire.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 QUESTION1="What's your name?"
+QUESTION2="Where are you from?"
 
 echo $QUESTION1
 read NAME
+
 
 echo Hello $NAME.
