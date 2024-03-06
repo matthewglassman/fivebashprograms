04cb762bb4aa7036188920a7a0643b7e621e89ab
Matthew Glassman <matthewglassman78@gmail.com>
2024-03-06 00:47:57 +0000

    Save progress: 700.1

--- countdown.sh
+++ countdown.sh
@@ -4,11 +4,11 @@
 echo -e "\n~~ Countdown Timer ~~\n"
 if [[ $1 -gt 0 ]]
 then
-for (( i = $1; i >= 0; i-- ))
+: 'for (( i = $1; i >= 0; i-- ))
 do
   sleep 1
   echo $i
-done
+done '
 else
   echo Include a positive integer as the first argument.
 fi
